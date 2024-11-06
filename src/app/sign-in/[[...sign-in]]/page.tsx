d09ec4c09233f02bf4170c501b793052b7c26f56
Neel Chalke <chalkeavry12@gmail.com>
2024-11-07 01:06:59 +0530

    yolo

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -4,6 +4,7 @@ export default function Page() {
   return (
     <div className="flex min-h-screen items-center justify-center border-2 border-white">
       <SignIn />
+      <h1 className="text-white font-bold">Yolo</h1>
     </div>
   );
 }
